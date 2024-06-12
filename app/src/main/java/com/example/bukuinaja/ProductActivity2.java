40d74ded53df9b63d088620092e756c9d331e72f
TomatoS0uP85 <faizabror85@student.uns.ac.id>
2024-06-12 07:42:00 +0700

    product

--- app/src/main/java/com/example/bukuinaja/ProductActivity2.java
+++ app/src/main/java/com/example/bukuinaja/ProductActivity2.java
@@ -1,4 +1,4 @@
-package com.example.bukuinaja.activity;
+package com.example.bukuinaja;
 
 import android.os.Bundle;
 
@@ -8,16 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.bukuinaja.R;
-
-public class AddProductActivity extends AppCompatActivity {
+public class ProductActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_product1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.product_activity), (v, insets) -> {
+        setContentView(R.layout.activity_product2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
