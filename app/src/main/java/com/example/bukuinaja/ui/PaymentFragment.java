2963fb3c66dcd5950a53e4950af1d3200ecf1a52
1Ly45 <137293380+1Ly45@users.noreply.github.com>
2024-06-12 07:02:30 +0700

    update

--- app/src/main/java/com/example/bukuinaja/ui/PaymentFragment.java
+++ app/src/main/java/com/example/bukuinaja/ui/PaymentFragment.java
@@ -1,25 +1,42 @@
 package com.example.bukuinaja.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.bukuinaja.R;
+import com.example.bukuinaja.activity.AddPaymentActivity;
 import com.example.bukuinaja.databinding.FragmentPaymentBinding;
 
 public class PaymentFragment extends Fragment {
+
+    Button btn;
 
     private FragmentPaymentBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPaymentBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View v = binding.getRoot();
 
-        return root;
+        btn = (Button) v.findViewById(R.id.product_add);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddPaymentActivity.class);
+                startActivity(i);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+
+            }
+        });
+        return v;
     }
 
     @Override
